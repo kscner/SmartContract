10e1b8215f89a0ed813cdbbe13e4034b2845a184
kscner <42165361+kscner@users.noreply.github.com>
2018-10-12 20:45:12 +0800

    OK

--- js/init.js
+++ js/init.js
@@ -3,7 +3,8 @@
  */
 var address = "0xfa0662d91daf5ebb0c90f36e2975cc5bde75aa70";
 
-window.addEventListener('load', function () {
+
+	console.log(11);
 	web3 = new Web3(Web3.givenProvider);
 	// Checking if Web3 has been injected by the browser (Mist/MetaMask)
 	if (typeof web3 !== 'undefined') {
@@ -15,8 +16,9 @@ window.addEventListener('load', function () {
 	} else {
 	  console.log('No web3? You should consider trying MetaMask!')
 	}
-  });
-  var contract = web3.eth.contract(abi).at(address);
+
+	console.log(22);
+  var contract = web3.eth.Contract(abi).at(address);
 	// var player;
 	// contract.getPlayer(function(error,result){
 	// 	console.log(result);
